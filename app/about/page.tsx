73c6df007b588063df2f0043096255e629befd1c
Shimadakunn <lcombaret@hotmail.co.jp>
2024-01-10 00:55:58 +0100

    commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,8 @@ const About = () => {
             <img src='../img/dounia.png' alt='Dounia' className='h-[60vh]' />
             <div className="w-[20vw] text-base space-y-4">
               <h1 className='text-5xl'>About Me</h1>
-                <p>Bienvenue dans l'univers visuel de Dounia, une artiste passionnée par l'art de la photographie. Basée à Paris, qui explor le monde à travers son objectif, capturant des moments intemporels et créant des images qui racontent des histoires.</p>
-                <p>Explorez le monde à travers l'objectif unique de mon travail et plongez-vous dans une expérience visuelle captivante. Chaque image est une invitation à voir le monde sous un nouvel angle, à travers les yeux d'une artiste passionnée par la magie de la photographie.</p>
+                <p>Bienvenue dans lunivers visuel de Dounia, une artiste passionnée par lart de la photographie. Basée à Paris, qui explor le monde à travers son objectif, capturant des moments intemporels et créant des images qui racontent des histoires.</p>
+                <p>Explorez le monde à travers lobjectif unique de mon travail et plongez-vous dans une expérience visuelle captivante. Chaque image est une invitation à voir le monde sous un nouvel angle, à travers les yeux dune artiste passionnée par la magie de la photographie.</p>
             </div>
     </div>
   )
